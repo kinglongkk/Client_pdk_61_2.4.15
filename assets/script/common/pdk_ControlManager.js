954cc498e2f39d9a14e9b0c7c8a5bb9bf004353f
子龙缘 <1406733029@qq.com>
2025-08-24 17:40:03 +0800

    子游戏

---
+++ assets/script/common/pdk_ControlManager.js
@@ -0,0 +1,154 @@
+/*
+    控制器管理器
+*/
+var app = require("pdk_app");
+
+var pdk_ControlManager = app.BaseClass.extend({
+
+    Init:function(){
+        this.JS_Name = app["subGameName"] + "_ControlManager";
+        
+        cc.game.on(cc.game.EVENT_HIDE,  this.OnEventHide.bind(this));
+        cc.game.on(cc.game.EVENT_SHOW,  this.OnEventShow.bind(this));
+
+        this.catchDataDict = {};
+        // bundle 缓存（例如 'pdk'）
+        this.subBundleName = app.subGameName;
+        this.subBundle = cc.assetManager.getBundle(this.subBundleName) || null;
+    },
+
+    //--------------回掉函数---------------
+    //应用切入后台
+    OnEventHide:function(){
+        app[app.subGameName + "Client"].OnEventHide();
+        
+    },
+
+    //应用显示
+    OnEventShow:function(){
+        app[app.subGameName + "Client"].OnEventShow();
+    },
+
+    //---------------加载资源-----------------
+
+    //创建异步下载资源对象
+    CreateLoadPromise:function(resPath, resType=""){
+        let that = this;
+        // 如果已经加载
+        if (this.catchDataDict.hasOwnProperty(resPath)) {
+            let loadData = this.catchDataDict[resPath];
+            return app.bluebird.resolve(loadData)
+        }
+
+        // 创建异步函数
+        let promisefunc = function (resolve, reject) {
+            const finalResType = resType || undefined;
+
+            const onLoaded = function (error, loadData) {
+                if (error) {
+                    reject(error);
+                    that.ErrLog("CreateLoadPromise failed resPath(%s) resType(%s), error:%s", resPath, resType, error.stack);
+                    return;
+                }
+                that.catchDataDict[resPath] = loadData;
+                resolve(loadData);
+            };
+
+            const loadFromResources = function () {
+                cc.resources.load(resPath, finalResType, onLoaded);
+            };
+
+            const loadFromBundle = function (bundle) {
+                try {
+                    bundle.load(resPath, finalResType, onLoaded);
+                } catch (e) {
+                    // 意外异常时回退到 resources
+                    that.ErrLog("bundle.load exception, fallback to resources. resPath(%s) err:%s", resPath, e.stack || e);
+                    loadFromResources();
+                }
+            };
+
+            // 优先使用子游戏 bundle（如 'pdk'），失败则回退到 resources
+            if (that.subBundle) {
+                loadFromBundle(that.subBundle);
+            } else {
+                cc.assetManager.loadBundle(that.subBundleName, function (err, bundle) {
+                    if (err || !bundle) {
+                        that.ErrLog("loadBundle(%s) failed, fallback to resources. err:%s", that.subBundleName, err && (err.stack || err));
+                        loadFromResources();
+                        return;
+                    }
+                    that.subBundle = bundle;
+                    loadFromBundle(bundle);
+                });
+            }
+        };
+        // 返回异步对象
+        return new app.bluebird(promisefunc);
+    },
+
+    //加载JSON文件 cc.url.raw('resources/json/' + jsonFileName + ".json");
+    CreateLoadPromiseByUrl:function(resPath){
+        let that = this;
+
+        //创建异步函数
+        let promisefunc = function(resolve, reject){
+            //加载资源
+            cc.assetManager.loadRemote(resPath, function (error, loadData) {
+
+                if(error){
+                    reject(error);
+                    return
+                }
+
+                resolve(loadData);
+            })};
+        //返回异步对象
+        return new app.bluebird(promisefunc);   
+    },
+
+    //创建异步下载文件夹所有文件对象
+    CreateLoadDirPromise:function(resPath, resType=""){
+        //创建异步函数
+        let promisefunc = function(resolve, reject){
+            //加载资源
+            cc.loader.loadResAll(resPath, resType, function (error, loadDataList) {
+
+                if(error){
+                    reject(error);
+                    return
+                }
+
+                resolve(loadDataList);
+            })};
+        //返回异步对象
+        return new app.bluebird(promisefunc);
+    },
+
+    //---------------释放资源-----------------
+    ReleaseAllRes:function(){
+        for(var key in this.catchDataDict){
+            console.log("ReleaseAllRes key:" + key);
+            if (key.indexOf("jsonData") > -1) {
+                //只释放配表资源
+                cc.loader.releaseRes(key);
+            }
+        }
+        this.catchDataDict = {};
+        cc.game.off(cc.game.EVENT_HIDE,  this.OnEventHide.bind(this));
+        cc.game.off(cc.game.EVENT_SHOW,  this.OnEventShow.bind(this));
+    },
+});
+
+
+var g_pdk_ControlManager = null;
+
+/**
+ * 绑定模块外部方法
+ */
+exports.GetModel = function(){
+    if(!g_pdk_ControlManager){
+        g_pdk_ControlManager = new pdk_ControlManager();
+    }
+    return g_pdk_ControlManager;
+}
---
+++ assets/script/net/pdk_NetRequest.js
@@ -0,0 +1,115 @@
+/*
+ *  ----------------------------------------------------------------------------------------------------
+ *  @copyright: Copyright (c) 2004, 2010 Xiamen DDM Network Technology Co.,Ltd., All rights reserved.
+ *  ----------------------------------------------------------------------------------------------------
+ *  @package NetRequest.js
+ *  @todo: 发送接收http
+ *  
+ *  @author hongdian
+ *  @date 2014-10-30 16:04
+ *  @version 1.0
+ *  
+ *  修改时间 修改人 修改内容
+ *  -------------------------------------------------------------------------------
+ *  
+ */
+var app = require("pdk_app");
+
+/**
+ * 类方法定义
+ */
+var pdk_NetRequest = app.BaseClass.extend({
+
+	Init:function(){
+		this.JS_Name = app.subGameName + "_NetRequest";
+	},
+
+	// 格式化HTTP请求日志
+	FormatHttpRequestLog: function(serverUrl, argString, requestType, sendPack, dataStr) {
+		// 简单的HTTP请求日志
+		const timestamp = new Date().toISOString();
+		console.log(`[${timestamp}] [HTTP_REQUEST] HTTP请求详情`);
+		console.log("请求URL:", serverUrl);
+		console.log("请求参数:", argString);
+		console.log("请求类型:", requestType);
+		console.log("请求数据:", JSON.stringify(sendPack, null, 2));
+		console.log("请求数据字符串:", dataStr);
+		console.log("请求数据大小:", dataStr.length, "字符");
+		console.log("完整URL:", [serverUrl, argString].join(""));
+	},
+
+
+	/**
+	 * 发送HTTP请求
+	 * * @param requestType POST or GET
+	 */
+	SendHttpRequest:function(serverUrl, argString, requestType, sendPack){
+
+		var url = [serverUrl, argString].join("")
+
+	 	var dataStr = JSON.stringify(sendPack);
+	 	let timeOut=false;
+		
+		// 格式化打印HTTP请求详情
+		this.FormatHttpRequestLog(serverUrl, argString, requestType, sendPack, dataStr);
+		
+		//每次都实例化一个，否则会引起请求结束，实例被释放了
+		var httpRequest = new XMLHttpRequest();
+		httpRequest.timeout = 3000;
+		httpRequest.open(requestType, url, true);
+		//服务器json解码
+		httpRequest.setRequestHeader("Content-Type", "application/json");
+		//httpRequest.setRequestHeader("Content-Length", dataStr.length);
+
+		//['loadstart', 'abort', 'error', 'load', 'loadend', 'timeout'].forEach(function (eventname) {
+		//	httpRequest["on" + eventname] = function () {
+		//		cc.log("\nEvent : " + eventname);
+		//	}
+		//});
+
+		var that = this;
+		httpRequest.onerror = function(){
+			that.ErrLog("httpRequest.error:%s", url);
+			app[app.subGameName + "_NetManager"]().OnConnectHttpFail(serverUrl, httpRequest.readyState, httpRequest.status);
+		};
+		httpRequest.ontimeout = function(){
+			timeOut=true;
+            app[app.subGameName + "_NetManager"]().OnConnectHttpFail(serverUrl, httpRequest.readyState, httpRequest.status);
+        };
+		httpRequest.onreadystatechange = function(){
+			if(timeOut==true){
+				return;
+			}
+			//执行成功
+			if (httpRequest.status == 200){
+				if(httpRequest.readyState == 4){
+					app[app.subGameName + "_NetManager"]().OnReceiveHttpPack(serverUrl, httpRequest.responseText);
+				}
+			}
+			else{
+				app[app.subGameName + "_NetManager"]().OnConnectHttpFail(serverUrl, httpRequest.readyState, httpRequest.status);
+				that.ErrLog("onreadystatechange(%s,%s)", httpRequest.readyState, httpRequest.status);
+			}
+		};
+		httpRequest.send(dataStr);
+		
+	},
+
+
+})
+
+
+
+//定义一个全局http
+var g_pdk_httpRequest = null 
+
+
+/**
+ * 绑定模块外部方法
+ */
+exports.GetModel = function(){
+    if(!g_pdk_httpRequest){
+        g_pdk_httpRequest = new pdk_NetRequest();
+    }
+    return g_pdk_httpRequest;
+}
